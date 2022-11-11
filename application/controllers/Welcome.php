949500c72fab0a71d7e89e90e3f6331c9c47b712
Arrohim Dwi Ksatria <76282186+dwiadisa@users.noreply.github.com>
2022-11-11 22:03:28 +0700

    balasan dari windows

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -22,5 +23,6 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 		echo "halo ini aku";
+		echo "ini dari windows";
 	}
 }
